07f3695a6f4ad50f6e295d3c3a50945a78f7013c
psshoon <psshoon123@naver.com>
2019-06-03 17:10:36 +0900

    코루틴 추가
    게임 진행 코루틴 추가.

--- Sonika/Assets/Scripts/GameManager.cs
+++ Sonika/Assets/Scripts/GameManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public IEnumerator gameStartRoutain;
+    public IEnumerator gameProcessRoutain;
+    public IEnumerator gameEndingRoutain;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
