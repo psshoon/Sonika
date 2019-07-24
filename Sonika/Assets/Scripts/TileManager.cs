b66f19537cf975adf5ef9dc4577bd24bcd1a7ab1
psshoon <psshoon123@naver.com>
2019-07-24 14:01:59 +0900

    123
    123

--- Sonika/Assets/Scripts/TileManager.cs
+++ Sonika/Assets/Scripts/TileManager.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    List<GameObject> tilesList = new List<GameObject>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
