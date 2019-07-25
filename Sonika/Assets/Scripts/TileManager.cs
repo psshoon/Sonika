4802ed50fcf404c1aacf0fa9bc3023ea64309c69
psshoon <psshoon123@naver.com>
2019-07-25 15:36:24 +0900

    tile instance 완료
    tile Manager로 tile instance 완료.

--- Sonika/Assets/Scripts/TileManager.cs
+++ Sonika/Assets/Scripts/TileManager.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    List<GameObject[]> tilesList = new List<GameObject[]>();
-
+    List<List<GameObject>> tilesList = new List<List<GameObject>>();
+    public int numberOfXtile = 0, numberOfZtile = 0;
 
     private void Awake()
     {
-        tilesList[0][0] = new GameObject();
+        makeTiles(numberOfXtile, numberOfZtile);
+    }
+
+    void makeTiles(int x, int z)
+    {
+        for(int i = 0; i < z; i++)
+        {
+            tilesList.Add(new List<GameObject>());
+            for (int j = 0; j < x; j++)
+            {
+                //GameObject tile;
+                //if (tile = Instantiate(Resources.Load("Prefeb/Tile") as GameObject))
+                //{
+                //    tile.transform.SetPositionAndRotation(new Vector3(j - 2, -1, i * 2), new Quaternion());
+                //    tilesList[i].Add(tile);
+                //}
+                //else
+                //{
+                //    Debug.Log("false");
+                //    break;
+                //}
+                GameObject tile = Instantiate(Resources.Load("Prefeb/Tile") as GameObject);
+                tile.transform.SetPositionAndRotation(new Vector3(j - 2, -1, i * 2), new Quaternion());
+                tilesList[i].Add(tile);
+
+            }
+        }
     }
 }
